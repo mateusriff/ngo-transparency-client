4ba3662b3819f05dded6467843ed25006d1203e0
Bruno Henrique <brunohenriquef.pinto@gmail.com>
2025-03-19 01:24:13 -0300

    inicializando front dos feeds pela bio
    feita a diferenciação de layout para bio na tela de usuarios externos e gestores de ongs, falta add os demais componentes do feed

--- src/components/feed/Feed.tsx
+++ src/components/feed/Feed.tsx
@@ -1,5 +1,9 @@
-import NgoView from "../../assets/pages/NgoView";
 import "./feed.css"
+import { FaInstagram, FaFacebook } from "react-icons/fa";
+import { FaXTwitter, FaPlus } from "react-icons/fa6";
+import Button from "../button/Button";
+import { useEffect, useState } from "react";
+
 
 interface feedProps {
     perfil: boolean;
@@ -14,23 +18,68 @@ interface feedProps {
 }
 
 
-function Feed({perfil, capa, name_ngo, x_link, facebook_link, instagram_link, pix_qr_code_link, ngo_logo, bio_description} : feedProps) {
-    return(
-            <main>
-                <section className="capa-feed">
-                    <img src={capa} alt="capa da página" />
-                </section>
+function Feed({ perfil, capa, name_ngo, x_link, facebook_link, instagram_link, pix_qr_code_link, ngo_logo, bio_description }: feedProps) {
 
+    const [isWideScreen, setIsWideScreen] = useState(window.innerWidth >= 481);
+
+    useEffect(() => {
+        const handleResize = () => setIsWideScreen(window.innerWidth >= 481);
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+
+    return (
+        <>
+            <section className="capa-feed">
+                <img src={capa} alt="capa da página" />
+            </section>
+            <main className="main-feed">
                 <section className="bio-feed">
-                    <div>
-                        <img src={ngo_logo} alt="logo da ong"/>
-                        <div>
-                            <h2>{name_ngo}</h2>   
+                    
+                    {perfil && isWideScreen ? <div className="horizontal-bio-feed">
+                        <img src={ngo_logo} alt="logo da ong" />
+                        <div className="vertical-bio-feed">
+                            <h3>{name_ngo}</h3>
+                            <div className="container-redes">
+                                <a className="face-icon" href={facebook_link}><FaFacebook />
+                                </a>
+                                <a className="insta-icon" href={instagram_link}><FaInstagram />
+                                </a>
+                                <a className="x-icon" href={x_link}><FaXTwitter />
+                                </a>
+                            </div>
                         </div>
                     </div>
 
+                        :
+
+                        <div className="container-bio">
+                            <img src={ngo_logo} alt="logo da ong" />
+                            <div className="name-icons-feed">
+                                <h3>{name_ngo}</h3>
+                                <div className="container-redes">
+                                    <a className="face-icon" href={facebook_link}><FaFacebook />
+                                    </a>
+                                    <a className="insta-icon" href={instagram_link}><FaInstagram />
+                                    </a>
+                                    <a className="x-icon" href={x_link}><FaXTwitter />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    }
+
+                    <p className={perfil ? "p-description" : "p-description p-description-center"}>{bio_description}</p>
+
+                    {perfil && <Button text="Adicionar" variant="primary" icon={<FaPlus />
+}/>}
+
+
                 </section>
             </main>
+        </>
+
     )
 }
 
